8896ad6a58404aed6b1b72ff2ffeec7540d76089
blegal <bertrand.legal@ims-bordeaux.fr>
2021-06-22 17:24:01 +0200

    Update radio.h

--- src/radio.h
+++ src/radio.h
@@ -18,7 +18,7 @@ class Radio{
 private :
 	float fc;
 	float fe;
-    int N;  //nbre ech
+//    int N;  //nbre ech
 	uhd::usrp::multi_usrp::sptr usrp;              // Le pointeur vers l'USRP
     uhd::rx_streamer::sptr rx_stream;
 
@@ -34,7 +34,7 @@ public :
 Radio::Radio(float s_fc, float s_fe){
     fc = s_fc;
     fe = s_fe;
-    N = 200000; //N = 200000 => 50 ms
+//    N = 200000; //N = 200000 => 50 ms
 }
 
 Radio::~Radio(){}
@@ -42,14 +42,15 @@ Radio::~Radio(){}
 void Radio::initialize(){
 	printf("%s",KYEL);
 
-    string usrp_addr("type=b100");           // L'adresse de l'USRP est écrite en dur pour l'instant
+    string usrp_addr("type=b200");           // L'adresse de l'USRP est écrite en dur pour l'instant
 	usrp = uhd::usrp::multi_usrp::make(usrp_addr); // Initialisation de l'USRP
 
 	usrp->set_rx_rate(fe);                         // Set de la fréquence d'échantillonnage
 	usrp->set_rx_freq(fc);                         // Set de la fréquence porteuse
 	//usrp->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("AB:0"));
 	usrp->set_rx_antenna("RX2");
-
+	usrp->set_rx_gain( 40 );
+	
 	uhd::stream_args_t
 	stream_args("fc32");        // Type des données à échantillonner (ici complexes float 64 - 32 bits par voie)
 	rx_stream = usrp->get_rx_stream(stream_args); // Pointeur sur les data reçues
@@ -64,11 +65,13 @@ void Radio::initialize(){
 	printf("%s", KNRM);
 }
 
-void Radio::reception(vector<complex<float> >* buffer){
+void Radio::reception(vector<complex<float> >* buffer)
+{
     uhd::rx_metadata_t md;                     // Des metadata
     int num_rx_samps = 0;                     // Nombre d'echantillons reçus
+    int N = buffer->size();
 
-    while (num_rx_samps < buffer->size())
+    while (num_rx_samps < N)
         num_rx_samps += rx_stream->recv(&buffer->at(num_rx_samps), N - num_rx_samps, md);
 }
 
